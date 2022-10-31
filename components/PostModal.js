718e2518138204af0a39f934825113fc3dded517
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-10-31 15:52:09 +0000

    modal width updated

--- components/PostModal.js
+++ components/PostModal.js
@@ -4,35 +4,40 @@ import Modal from "react-modal";
 import selectedPostState from "../atoms/selectedPostAtom.js";
 import Post from "./Post.js";
 
-const customStyles = {
-  content: {
-    top: "54%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    padding: "0",
-    borderRadius: "12px",
-    border: "none",
-    zIndex: "100",
-    width: "84vw",
-    height: "80%",
-  },
-  overlay: {
-    backgroundColor: "rgb(27 26 26 / 70%)",
-  },
-};
-
 const PostModal = ({ openPostModal, setOpenPostModal }) => {
   const [selectedPost, setSelectedPost] = useRecoilState(selectedPostState);
+  let modalWidth;
+  let modalHeight;
+
+  if (typeof window !== "undefined") {
+    modalWidth = window.innerWidth < 744 ? "84vw" : "50%";
+    modalHeight = window.innerWidth < 744 ? "80%" : "auto";
+  }
 
   return (
     <Modal
       isOpen={openPostModal}
       onRequestClose={() => setOpenPostModal(false)}
       ariaHideApp={false}
-      style={customStyles}
+      style={{
+        content: {
+          top: "54%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          padding: "0",
+          borderRadius: "12px",
+          border: "none",
+          zIndex: "100",
+          width: modalWidth,
+          height: modalHeight,
+        },
+        overlay: {
+          backgroundColor: "rgb(27 26 26 / 70%)",
+        },
+      }}
       contentLabel="Post"
       shouldCloseOnOverlayClick
     >
